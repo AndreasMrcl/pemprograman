83a4de1188f4d9b73255ff2ecf31ffe883d7bc34
AndreasMrcl <andreasmrcl2@gmail.com>
2022-03-30 15:31:33 +0700

    done

---
+++ Belajar-php-oop/data/Student.php
@@ -0,0 +1,24 @@
+<?php
+
+class Student
+{
+    public string $id;
+    public string $name;
+    public int $value;
+    private string $sample;
+
+    public function setSample(string $sample): void
+    {
+        $this->sample = $sample;
+    }
+
+    public function __clone()
+    {
+        unset($this->sample);
+    }
+
+    public function __toString(): string
+    {
+        return "Student id : $this->id, name : $this->name, value : $this->value";
+    }
+}
