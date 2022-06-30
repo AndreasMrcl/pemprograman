a7880836a4ade0f3963aeb0f2d712e0c10cce266
AndreasMrcl <andreasmrcl2@gmail.com>
2022-06-30 17:12:41 +0700

    .

--- semester-04/Web-Program/item/sidebar.php
+++
@@ -1,6 +0,0 @@
-<div class="sidebar">
-    <h1>Program CRUD</h1>
-    <a href="../index.php">Dashboard</a>
-    <a href="<?= dirname($_SERVER['PHP_SELF']) . "/item" ?>">Barang</a>
-    <a href="#">Logout</a>
-</div>
---
+++ semester-04/Web-Program/tugas10-06492-andreas-A12.6406/item/sidebar.php
@@ -0,0 +1,6 @@
+<div class="sidebar">
+    <h1>Administrator</h1>
+    <a href="../index.php">Dashboard</a>
+    <a href="<?= dirname($_SERVER['PHP_SELF']) . "/item" ?>">Barang</a>
+    <a href="#">Logout</a>
+</div>
